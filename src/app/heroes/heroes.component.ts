9746b44e45decaf5f495a2f563a89054b22496b8
PythonMonty <andrewnovosel90@gmail.com>
2024-05-22 15:27:37 -0400

    Step 4 completed and running smoothly

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
-import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  standalone: false,
-  /*
-  imports: [FormsModule, NgIf, NgFor, UpperCasePipe], //TS-992010: 'imports' is only valid on a component that is standalone.
-  */
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Spider-Man' },
+  { id: 13, name: 'Wolverine' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Deadpool' },
+  { id: 16, name: 'Ant-Man' },
+  { id: 17, name: 'Captain America' },
+  { id: 18, name: 'Star-Lord' },
+  { id: 19, name: 'Hulk' },
+  { id: 20, name: 'Thor' },
 ];
