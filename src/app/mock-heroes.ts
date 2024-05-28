9a6c11e05e0cff8029577cb1ea9927426f3e1b35
PythonMonty <andrewnovosel90@gmail.com>
2024-05-28 15:00:50 -0400

    Made changes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,49 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Spider-Man' },
-  { id: 13, name: 'Wolverine' },
-  { id: 14, name: 'Iron Man' },
-  { id: 15, name: 'Deadpool' },
-  { id: 16, name: 'Ant-Man' },
-  { id: 17, name: 'Captain America' },
-  { id: 18, name: 'Star-Lord' },
-  { id: 19, name: 'Hulk' },
-  { id: 20, name: 'Thor' },
+  {
+    id: 12,
+    name: 'Spider-Man',
+    info: 'Spider-Man is a resilient and relatable superhero who swings through the streets of New York City, balancing his dual identities as the friendly neighborhood Peter Parker and the web-slinging protector of the innocent.',
+  },
+  {
+    id: 13,
+    name: 'Wolverine',
+    info: 'Wolverine, a brooding and enigmatic mutant with a troubled past, possesses an unbreakable adamantium skeleton and razor-sharp claws, making him a fierce and relentless warrior.',
+  },
+  {
+    id: 14,
+    name: 'Iron Man',
+    info: 'Iron Man, the charismatic genius billionaire Tony Stark, uses his brilliant mind and cutting-edge technology to armor up as the iconic superhero, defending the world from threats both terrestrial and cosmic with wit, ingenuity, and an unwavering commitment to justice.',
+  },
+  {
+    id: 15,
+    name: 'Deadpool',
+    info: 'Deadpool, the irreverent and unpredictable "Merc with a Mouth," combines deadly combat skills, accelerated healing, and a penchant for breaking the fourth wall to deliver a unique blend of humor and chaos while slicing through his enemies with his signature katana blades.',
+  },
+  {
+    id: 16,
+    name: 'Ant-Man',
+    info: 'Ant-Man, the witty and resourceful Scott Lang, harnesses the astonishing power of Pym Particles to shrink to the size of an ant while retaining superhuman strength, embarking on epic adventures and proving that even the smallest hero can make a big impact.',
+  },
+  {
+    id: 17,
+    name: 'Captain America',
+    info: 'Captain America, the embodiment of honor and heroism, epitomizes the ideals of courage, justice, and sacrifice as he wields his iconic shield to defend freedom and stand against tyranny, inspiring hope and unity in the face of adversity.',
+  },
+  {
+    id: 18,
+    name: 'Star-Lord',
+    info: 'Star-Lord, the charismatic and roguish leader of the Guardians of the Galaxy, navigates the cosmos with a mix of charm, cunning, and a killer soundtrack, wielding his dual blasters and strategic wit to protect the galaxy while searching for his own place in the universe.',
+  },
+  {
+    id: 19,
+    name: 'Hulk',
+    info: 'A colossal and uncontrollable force of raw power, embodies the struggle between man and monster as scientist Bruce Banner grapples with his inner demons while unleashing earth-shattering strength and fury as the green-skinned behemoth, smashing through any obstacle in his path.',
+  },
+  {
+    id: 20,
+    name: 'Thor',
+    info: 'The mighty god of thunder, wields the enchanted hammer Mjolnir and commands the power of storms, defending the realms with valor and honor while navigating the complexities of his dual identity as both a divine warrior and a humble prince of Asgard.',
+  },
 ];
